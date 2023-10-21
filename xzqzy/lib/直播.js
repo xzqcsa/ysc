859ac61b58e5c3afa615bb942ea7f4bc0c4b4158
徐先生 <13597392+xzqaaa@user.noreply.gitee.com>
2023-10-21 06:15:10 +0000

    update js.
    Signed-off-by: 徐先生 <13597392+xzqaaa@user.noreply.gitee.com>

---
+++ xzqzy/lib/直播.js
@@ -0,0 +1,10 @@
+[
+ {"name": "MV合集",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/mv.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+ {"name": "MTV合集",     "url": "https://qu.ax/KmzB.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+{"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"}
+]
