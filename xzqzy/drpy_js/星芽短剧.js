c003d3110b4a2347673651aa6b2a03b63476b544
xzqcsa <494909361@qq.com>
2024-09-19 08:29:04 +0800

    1

--- xzqzy/drpy_js/星芽短剧.js
+++ xzqzy/drpy_js/星芽短剧.js
@@ -1,67 +1,111 @@
+ 
 var rule = {
-    类型: '影视',
-    title: '星芽短剧[优]',
+    title: '星芽短剧',
     host: 'https://app.whjzjx.cn',
-    url: '/cloud/v2/theater/home_page?theater_class_id=fyclass&type=1&class2_ids=0&page_num=fypage&page_size=24',
-//    https://app.whjzjx.cn/cloud/v2/theater/home_page?theater_class_id=1&type=1&class2_ids=0&page_num=1&page_size=24
-//https://app.whjzjx.cn/cloud/v2/theater/home_page?theater_class_id=1&type=1&class2_ids=0&page_num=2&page_size=24
-    searchUrl: '/v3/search',
+    url: '/v1/theater/home_page?theater_class_id=1&class2_id=fyclass&page_num=fypage&page_size=24',
+    searchUrl: '/?**',
     searchable: 2,
-    quickSearch: 0,
-    headers: {
-        'User-Agent': 'okhttp/4.10.0',
-        'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjY1MTQ3MzYsIlVzZXJJZCI6NTcxMjk0OTgsInJlZ2lzdGVyX3RpbWUiOiIyMDI0LTA5LTAyIDAzOjI1OjM2IiwiaXNfbW9iaWxlX2JpbmQiOmZhbHNlfQ.umYgwq2KknucsWGImHSJEEKpRXol6Mu7S6YSZlgb26Q'
-    },
-    timeout: 5000,
-    class_name: '剧场&热播剧&星选好剧&新剧&阳光剧场',
-    class_url: '1&2&7&3&5',
-    play_parse: true,
-    lazy: $js.toString(() => {
-        input = {url: input, parse: 0}
-    }),
-    double: true,
+    quickSearch: 1,
+    // filterable: 1,
+    class_name: '热播剧&星选好剧&新剧&阳光剧场',
+    class_url: '2&7&3&5',
     一级: $js.toString(() => {
         let d = [];
-        let html = request(input);
-        let data = JSON.parse(html).data.list;
-        data.forEach(it => {
+        let urlk = `https://app.whjzjx.cn/v1/theater/home_page?theater_class_id=${MY_CATE}&page_num=${MY_PAGE - 1}&page_size=24`;
+        let tkurl = 'https://app.whjzjx.cn/v1/account/login';
+        let head = JSON.parse(post(tkurl, {
+            headers: {
+                "Connection": "keep-alive",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Content-Length": "40",
+                "user-agent": "okhttp/4.10.0",
+                "user_agent": "Mozilla/5.0 (Linux; Android 9; ASUS_I003DD Build/PI; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.70 Mobile Safari/537.36",
+                "Host": "app.whjzjx.cn",
+                "Accept-Encoding": "gzip"
+            },
+            body: "device=20caaae96b3443174bf4ebdbdcc253776"
+        })).data.token;
+        let header={"authorization":head};
+        let html = JSON.parse(request(urlk, {
+            headers: header
+        }));
+        let bata = html.data.list;
+        bata.forEach(it => {
             let id = 'https://app.whjzjx.cn/v2/theater_parent/detail?theater_parent_id=' + it.theater.id;
             d.push({
                 url: id,
                 title: it.theater.title,
                 img: it.theater.cover_url,
-                desc: it.theater.theme,
-            })
-        })
+                desc: it.theater.total + "集",
+                content: "播放量:" + it.theater.play_amount_str,
+            });
+        });
         setResult(d);
     }),
     二级: $js.toString(() => {
         let urls = [];
-        let html = request(input);
-        let data = JSON.parse(html).data;
+        let tkurl = 'https://app.whjzjx.cn/v1/account/login';
+        let head = JSON.parse(post(tkurl, {
+            headers: {
+                "Connection": "keep-alive",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Content-Length": "40",
+                "user-agent": "okhttp/4.10.0",
+                "user_agent": "Mozilla/5.0 (Linux; Android 9; ASUS_I003DD Build/PI; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.70 Mobile Safari/537.36",
+                "Host": "app.whjzjx.cn",
+                "Accept-Encoding": "gzip"
+            },
+            body: "device=20caaae96b3443174bf4ebdbdcc253776"
+        })).data.token;
+        let header={"authorization":head};
+        let data = JSON.parse(request(input, {
+            headers: header
+        })).data;
+        //console.log(data);
         data.theaters.forEach(it => {
-            urls.push(it.num + '$' + it.son_video_url);
-        })
+            urls.push("第" + it.num + "集" + '$' + it.son_video_url);
+        });
         VOD = {
             vod_name: data.title,
             vod_pic: data.cover_url,
-            vod_play_from: '完美',
+            vod_remarks: data.total + "集",
+            vod_play_from: '星芽短剧',
             vod_play_url: urls.join('#')
         };
     }),
     搜索: $js.toString(() => {
         let d = [];
-        let html = post(input, {body: {"text": KEY}})
-        let list = JSON.parse(html).data.theater.search_data;
-        list.forEach(it => {
+        let ht = input.match(/\?(.*)/)[1];
+        let ser = "https://app.whjzjx.cn/v2/search";
+        let tkurl = 'https://app.whjzjx.cn/v1/account/login';
+        let head = JSON.parse(post(tkurl, {
+            headers: {
+                "Connection": "keep-alive",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Content-Length": "40",
+                "user-agent": "okhttp/4.10.0",
+                "user_agent": "Mozilla/5.0 (Linux; Android 9; ASUS_I003DD Build/PI; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.70 Mobile Safari/537.36",
+                "Host": "app.whjzjx.cn",
+                "Accept-Encoding": "gzip"
+            },
+            body: "device=20caaae96b3443174bf4ebdbdcc253776"
+        })).data.token;
+        let header={"authorization":head};
+        let html = JSON.parse(post(ser, {
+            headers: header,
+            body: "text=" + ht
+        }));
+        let bata = html.data.search_data;
+        bata.forEach(it => {
             let id = 'https://app.whjzjx.cn/v2/theater_parent/detail?theater_parent_id=' + it.id;
             d.push({
                 url: id,
                 title: it.title,
                 img: it.cover_url,
-                content: it.introduction,
-            })
-        })
+                desc: it.score_str + "|" + it.total + "集",
+            });
+        });
         setResult(d);
     }),
-}
+};
+ 
